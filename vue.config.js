f09022d1db00e10c70e3f8408fabffad939c0628
Jin <88888888@qq.com>
2021-11-28 03:17:44 +0800

    再来

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
     lintOnSave: false,
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'https://elm.cangdu.org',
-          pathRewrite: {'^/api' : ''},
-          changeOrigin:true
-        },
-      },
-    },
     build:{
       assetsPublicPath:'./'
     }
